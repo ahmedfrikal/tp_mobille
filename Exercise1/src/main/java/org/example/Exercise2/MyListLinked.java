b2cebfa2128c42b6b947cc050ef48b1f75a8f739
FRIKAL Ahmed <ahmad.frikal@gmail.com>
2023-03-21 19:19:24 +0000

    Ajouter des attribut de class et methode get pour size de tableau

--- Exercise1/src/main/java/org/example/Exercise2/MyListLinked.java
+++ Exercise1/src/main/java/org/example/Exercise2/MyListLinked.java
@@ -1,4 +1,14 @@
 package org.example.Exercise2;
 
 public class MyListLinked {
+
+    //Taille de tableau
+    private int size;
+    private Node premierElement;
+    public MyListLinked(){
+        size=0;
+        premierElement=null;
+    }
+    public  int getSize(){return size;}
+    
 }
