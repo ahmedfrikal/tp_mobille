ce7de8496e26eadd7ab737d33bd8619097094ebe
FRIKAL Ahmed <ahmad.frikal@gmail.com>
2023-03-21 19:14:24 +0000

    Ajouter le contenu de page Node

--- Exercise1/src/main/java/org/example/Exercise2/Node.java
+++ Exercise1/src/main/java/org/example/Exercise2/Node.java
@@ -1,4 +1,18 @@
 package org.example.Exercise2;
 
 public class Node {
+    private int nombre;
+    private Node nodeNv;
+
+    public Node getNodeNv() {
+        return nodeNv;
+    }
+
+    public void setNodeNv(Node nodeNv) {
+        nodeNv = nodeNv;
+    }
+    public Node(int nombre){
+        this.nombre= nombre;
+        this.nodeNv= null;
+    }
 }
